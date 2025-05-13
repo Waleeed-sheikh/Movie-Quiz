5f87fa814a32992bca11ac9064b8ca32b1332e5a
Waleeed-sheikh <waleedshaikha779@gmail.com>
2025-05-13 20:04:31 +0500

    Railway configuration

--- movie-backend/server.js
+++ movie-backend/server.js
@@ -19,9 +19,8 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://waleeywood.netlify.app",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
-    credentials: true
   },
 });
 
@@ -354,7 +353,8 @@ function getDifficultyPoints(difficulty) {
   return pointsMap[difficulty.toLowerCase()] || 1;
 }
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 // //xmlhttprequest , axios, fetch, promises async await
